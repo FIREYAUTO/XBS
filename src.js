640620b22546a3907d767ed975447e53aa9adfe5
FIREYAUTO <56837610+FIREYAUTO@users.noreply.github.com>
2022-02-09 09:38:37 -0800

    Update src.js

--- src.js
+++ src.js
@@ -1751,6 +1751,7 @@ const XBS = ((DebugMode = false) => {
 				Next = this.Tokens[this.Position + 1];
 			if (!Next) return Expression.Value;
 			if (AST.IsToken(Next, "LINEEND", "Operator")) return Expression.Value;
+			if(AST.IsType(Next,"Identifier"))ErrorHandler.AError(this,"Unexpected","identifier while parsing complex expression");
 			for (let Complex of AST.ComplexExpressions) {
 				if (!AST.IsToken(Next, Complex.Value, Complex.Type)) continue;
 				if (Expression.Priority <= Complex.Priority) {
