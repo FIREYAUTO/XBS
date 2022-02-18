f7f23e249cab0a15664e383c8731498f2fb38568
FIREYAUTO <56837610+FIREYAUTO@users.noreply.github.com>
2022-02-18 08:24:53 -0800

    Update canvas.js

--- extensions/canvas.js
+++ extensions/canvas.js
@@ -8,10 +8,43 @@ Adds drawing components to XBS for HTML canvas elements
 	//{{ Variable References }}\\
 	
 	const Tokenizer=XBS.Tokenizer,
-		AST=XBS.AST,
-		Interpreter=XBS.Interpreter,
-		ErrorHandler=XBS.ErrorHandler,
-	      	IState=XBS.IState;
+		  AST=XBS.AST,
+		  Interpreter=XBS.Interpreter,
+		  ErrorHandler=XBS.ErrorHandler,
+		  IState=XBS.IState,
+		  ASTBase=XBS.ASTBase,
+		  ASTNode=XBS.ASTNode,
+		  ASTBlock=XBS.ASTBlock;
+	
+	function Lerp(a,b,t){
+		return (1-t)*a+t*b;
+	}
+	
+	class VectorBase {
+		constructor(){
+			return this;
+		}
+		Add(v){return this.Math(v,(a,b)=>a+b)}
+		Sub(v){return this.Math(v,(a,b)=>a-b)}
+		Mul(v){return this.Math(v,(a,b)=>a*b)}
+		Div(v){return this.Math(v,(a,b)=>a/b)}
+		Mod(v){return this.Math(v,(a,b)=>a%b)}
+		Pow(v){return this.Math(v,(a,b)=>a**b)}
+	}
+
+	class Vector extends VectorBase {
+		constructor(x=0,y=0){super();this.x=x;this.y=y}
+		get X(){return this.x}
+		get Y(){return this.y}
+		set X(v){return this.x=v}
+		set Y(v){return this.y=v}
+		toString(){return `${this.x},${this.y}`}
+		Math(v,c){let x=v,y=v;if(v instanceof Vector)x=v.x,y=v.y;return new Vector(c(this.x,x),c(this.y,y))}
+		get Magnitude(){return Math.sqrt((this.x**2)+(this.y**2))}
+		Normalize(){let m = this.Magnitude;return new Vector(this.x/m,this.y/m)}
+		NoNan(){let{x,y}=this;return new Vector(isNaN(x)?0:x,isNaN(y)?0:y)}
+		Lerp(v,t){return new Vector(Lerp(this.x,v.x,t),Lerp(this.y,v.y,t))}
+	}
 	
 	//{{ Internal AST Functions }}\\
 	
@@ -21,6 +54,26 @@ Adds drawing components to XBS for HTML canvas elements
 			Type:"Identifier",
 			Call:function(Stack){
 				let Node = Stack.NewNode("Line");
+				Stack.Next();
+				Node.Write("List",Stack.ParseExpression());
+				return Node;
+			},
+		},
+		{
+			Value:"stroke",
+			Type:"Identifier",
+			Call:function(Stack){
+				let Node = Stack.NewNode("Stroke");
+				Stack.Next();
+				Node.Write("List",Stack.ParseExpression());
+				return Node;
+			},
+		},
+		{
+			Value:"fill",
+			Type:"Identifier",
+			Call:function(Stack){
+				let Node = Stack.NewNode("Fill");
 				Stack.Next();
 				Node.Write("List",Stack.ParseExpression());
 				return Node;
@@ -76,12 +129,51 @@ Adds drawing components to XBS for HTML canvas elements
 	
 	const IStates = {
 		Line:function(Stack,State,Token){
-			
+			let C = State.Context,
+				List = Stack.Parse(State,Token.Read("List"));
+			let V1 = List[0],
+				V2 = List[1];
+			if(!(V1 instanceof __VECTOR))ErrorHandler.IError(Token,"Expected","Vector for line v1",Stack.GetType(V1));
+			if(!(V2 instanceof __VECTOR))ErrorHandler.IError(Token,"Expected","Vector for line v2",Stack.GetType(V2));
+			C.moveTo(V1.x,V1.y);
+			C.lineTo(V2.x,V2.y);
+		},
+		Path:function(Stack,State,Token){
+			let C = State.Context,
+				Type = Token.Read("Type");
+			if(Type==="Begin"){
+				C.beginPath();	
+			}else if(Type==="Close"){
+				C.closePath();
+			}
+		},
+		Stroke:function(Stack,State,Token){
+			let C = State.Context,
+				Style = Stack.Parse(State,Token.Read("Style"));
+			if(Style){
+				C.strokeStyle = Style;
+				C.stroke();	
+			}
+		},
+		Fill:function(Stack,State,Token){
+			let C = State.Context,
+				Style = Stack.Parse(State,Token.Read("Style"));
+			if(Style){
+				C.fillStyle = Style;
+				C.fill();	
+			}
 		},
 	};
 	
 	function ParseIState(Stack,State,Token){
-		
+		if(!(Token instanceof ASTBase))return Token;
+		for(let Name in IStates){
+			let Call = IStates[Name];
+			if(Token.Type===Name){
+				return Call(Stack,State,Token);
+			}
+		}
+		return null;
 	}
 	
 	function ParseCState(Stack,State){
