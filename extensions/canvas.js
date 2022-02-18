477e433480a2d66e92b3a3afd7b094caf5b28d7c
FIREYAUTO <56837610+FIREYAUTO@users.noreply.github.com>
2022-02-18 07:46:58 -0800

    Update canvas.js

--- extensions/canvas.js
+++ extensions/canvas.js
@@ -4,7 +4,13 @@ Adds drawing components to XBS for HTML canvas elements
 */
 
 (()=>{
-	//{{ Internal Functions }}\\
+	//{{ Variable References }}\\
+	const Tokenizer=XBS.Tokenizer,
+		AST=XBS.AST,
+		Interpreter=XBS.Interpreter,
+		ErrorHandler=XBS.ErrorHandler;
+	
+	//{{ Internal AST Functions }}\\
 	const States = [
 		{
 			Value:"line",
@@ -14,9 +20,30 @@ Adds drawing components to XBS for HTML canvas elements
 			},
 		},
 	];
+	function ParseState(Stack){
+		let Result = undefined;
+		let Token = Stack.Token;
+		for(let State of States){
+			if(){
+				
+			}
+		}
+		return Result;
+	}
 	function ParseBlock(Stack){
-		let Block = Stack.NewBlock("CanvasChunk");
-		
+		Stack.ErrorIfEOS();
+		if(AST.IsToken(Stack.Token,"BOPEN","")){
+			let Block = Stack.NewBlock("CanvasChunk");
+			while(!Stack.IsEnd()){
+				Stack.ErrorIfEOS();
+				if(Stack.CheckNext("BCLOSE","Bracket"))break;
+				this.Next();
+				Block.Write(ParseState(Stack));
+			}
+			Stack.TestNext("BCLOSE","Bracket");
+			Stack.Next();
+			return Block;
+		}else ErrorHandler.AError(Stack,"Expected","bracket {",`${Stack.Token.Type.toLowerCase()} ${Stack.Token.RawValue}`);
 	}
 	//{{ Canvas Syntax }}\\
 	XBS.NewToken("CANVAS","cdraw","Keyword");
